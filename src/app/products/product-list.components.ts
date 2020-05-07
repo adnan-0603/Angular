fca23137f182a28dc279cd60c3992d7a066bb9a7
chowdhury.mdadnan@gmail.com <chowdhury.mohammadadnan06@gmail.com>
2020-05-08 05:27:15 +0600

    Using Lifecyle Hooks

--- src/app/products/product-list.components.ts
+++ src/app/products/product-list.components.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {IProduct} from './product';
 @Component({
     selector: 'pm-products',
@@ -6,7 +6,7 @@ import {IProduct} from './product';
     styleUrls: ['./product-list.component.css']
 })
 
-export class ProductListComponent{
+export class ProductListComponent implements OnInit{
     pageTitle: string = "Product List";
     imageWidth: number =50;
     imageMargin: number =2;
@@ -38,6 +38,9 @@ export class ProductListComponent{
 
     toggleImage(): void{
         this.showImage =!this.showImage;
+    }
+    ngOnInit():void{
+        console.log("In OnInit");
     }
 
 }
